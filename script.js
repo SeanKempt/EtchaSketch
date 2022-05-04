a62047945cf2798553c3031a287dc529e887c332
“Kempt101” <Kempt101@gmail.com>
2022-05-04 08:58:12 -0500

    Add eventlisteners and color change effect

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let gridDivContainer = document.getElementById("gridContainer");
 let node = document.createTextNode("this is some text");
-
+//Creates grid squares inside the grid container
 function createGridDiv() {
   for (let i = 0; i <= 255; i++) {
     let gridDiv = document.createElement("div");
@@ -10,3 +10,11 @@ function createGridDiv() {
 }
 
 createGridDiv();
+
+let gridSquare = document.getElementsByClassName("gridSquares");
+
+for (let i = 0; i < gridSquare.length; i++) {
+  gridSquare[i].addEventListener("mouseover", function (event) {
+    event.target.style.backgroundColor = "black";
+  });
+}
